58b614869e00834bb00abedfe865d3da8a314b1e
williamfzc <178894043@qq.com>
2022-08-18 00:47:12 +0800

    chore(server): builtin tasks

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -1,6 +1,7 @@
 package server
 
 type Config struct {
-	Port  int  `json:"port"`
-	Debug bool `json:"debug"`
+	Port       int          `json:"port"`
+	Debug      bool         `json:"debug"`
+	InnerTasks []TaskDetail `json:"innerTasks"`
 }
